a0b66c35b77df493a6630f93f7e95529d14912d8
LeRoy Dahl <ldahl@longevity.inc>
2023-06-07 20:52:43 -0500

    Update build

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 import emailjs from 'emailjs-com';
 
-const CloseModalIcon = () => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+const CloseModalIcon = () => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 </svg>
 
